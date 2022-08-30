5a027c2a271b60bde60f81ac7b79e5bc19fde917
Hedayat Kamalian <hedayat.kamalian@gmail.com>
2022-08-30 15:35:07 +0430

    add enities and relations (#1)
    Co-authored-by: Hedayat <hedayat@coffeechatnetwork.io>

---
+++ CleanArchitect.Infrastructure/EFCore/Configs/CustomerConfig.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchitect.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchitect.Infrastructure.EFCore.Configs;
+
+public class CustomerConfig : IEntityTypeConfiguration<Customer>
+{
+    public void Configure(EntityTypeBuilder<Customer> builder)
+    {
+        builder.HasKey(p => p.Id);
+
+        builder.HasMany<Order>()
+            .WithOne()
+            .HasForeignKey(p => p.CustomerId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Property(p => p.FirstName).HasMaxLength(500).IsRequired();
+        builder.Property(p => p.LastName).HasMaxLength(500).IsRequired();
+        builder.Property(p => p.PhoneNumber).HasMaxLength(12).IsRequired();
+
+    }
+}
---
+++ CleanArchitect.Infrastructure/EFCore/Configs/OrderConfig.cs
@@ -0,0 +1,29 @@
+﻿using CleanArchitect.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchitect.Infrastructure.EFCore.Configs;
+
+public class OrderConfig : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.HasKey(p => p.Id);
+
+        builder.OwnsOne(p => p.Address);
+        builder.Property(p => p.Address).IsRequired();
+
+        builder.Ignore(p => p.TotalPrice);
+
+        builder.HasMany<OrderItem>(p => p.Items)
+            .WithOne()
+            .HasForeignKey(p => p.OrderId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne<Customer>()
+            .WithMany()
+            .HasForeignKey(p => p.CustomerId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+    }
+}
